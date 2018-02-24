adef12d48de52bf270a4270e5c9db3616c939e27
Martha Salamat <salamat.martha@gmail.com>
2018-02-24 16:20:58 +0800

    carkila update

---
+++ Carkila/app/src/main/java/com/example/admin/firebasestorageexample/VehicleList.java
@@ -0,0 +1,13 @@
+package com.example.admin.firebasestorageexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VehicleList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_list);
+    }
+}
