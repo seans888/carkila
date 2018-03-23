96333147878345f7a538158cb28a4ad019d34e12
Martha Salamat <salamat.martha@gmail.com>
2018-03-23 16:02:33 +0800

    remove

--- Carkila/app/src/main/java/com/example/admin/firebasestorageexample/Messages.java
+++
@@ -1,8 +0,0 @@
-package com.example.admin.firebasestorageexample;
-
-/**
- * Created by Martha on 25/02/2018.
- */
-
-class Messages {
-}
--- Carkila/app/src/main/java/com/example/admin/firebasestorageexample/Profile.java
+++
@@ -1,8 +0,0 @@
-package com.example.admin.firebasestorageexample;
-
-/**
- * Created by Martha on 19/02/2018.
- */
-
-class Profile {
-}
