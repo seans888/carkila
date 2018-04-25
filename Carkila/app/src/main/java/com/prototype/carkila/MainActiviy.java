780799bc2b20217776464e7bbff8b4718ec1b676
Martha Salamat <salamat.martha@gmail.com>
2018-04-25 22:19:17 +0800

    remove folder, updated FDD

--- Carkila/app/src/main/java/com/prototype/carkila/MainActiviy.java
+++
@@ -1,8 +0,0 @@
-package com.prototype.carkila;
-
-/**
- * Created by Martha on 09/04/2018.
- */
-
-class MainActiviy {
-}
--- Carkila/app/src/main/java/com/prototype/carkila/Vehicle/VehicleInfo.java
+++
@@ -1,8 +0,0 @@
-package com.prototype.carkila.Vehicle;
-
-/**
- * Created by Martha on 07/04/2018.
- */
-
-class VehicleInfo {
-}
